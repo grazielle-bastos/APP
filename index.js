d362bc10e8abe96aae6d5437bb2fd93e855cd4c6
Grazielle Bastos <grazielle1101@gmail.com>
2024-09-10 22:16:28 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -2,17 +2,15 @@
 
 let meta = {
     value: "ler um livro por mês",
-    address: 2,
     checked: false,
-    log: (info) => {
-        console.log(info)
-    }
 }
 
-meta.value = "não é mais ler um livro"
-meta.log(meta.value)
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todos os dias",
+        checked: false
+    }
+]
 
-// function // arrow fuction
-// const criarMeta = () => {}
-
-function criarMeta() {}
+console.log(metas[1].value)
